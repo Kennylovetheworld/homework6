075fc2aedf4009d61acf0349ccb82b2133dfb727
Kennylovetheworld <944545895@qq.com>
2020-02-28 15:19:43 -0500

    second commit

--- js/video.js
+++ js/video.js
@@ -5,48 +5,62 @@ function getVid(){
 }
 
 function playVid() { 
-
+	document.querySelector("#volume").innerHTML = video.volume * 100 + '%';
+	video.play();
 	console.log("Play Video");
 } 
 
 function pauseVid() { 
-
+	video.pause();
 	console.log("Pause Video");
 } 
 
 function decreaseSpeed() { 
-
-  	console.log("Speed is "+ );
+	video.playbackRate *= 0.8;
+  	console.log("Speed is "+ video.playbackRate);
 } 
 
 function increaseSpeed() {
-
-	console.log("Speed is "+ );
+	video.playbackRate *= 1.25;
+	console.log("Speed is "+ video.playbackRate);
 } 
 
 function skipAhead() {
-
-	console.log("Current location is "+ );
+	video.currentTime += 60;
+	if(video.currentTime == video.duration)
+		video.currentTime = 0;
+		video.playbackRate = 1;
+	console.log("Current location is "+ video.currentTime);
 } 
 
 function mute() { 
-  	
-  		console.log("Unmuted");
-  	
+	var  m = document.querySelector("#mute");
+  	if(video.muted){
+		m.innerHTML = "Mute";
+		video.muted = false;
+		console.log("Unmuted");
+	}
+  	else{
+		m.innerHTML = "Unmute";
+		video.muted = true;
       	console.log("Muted");
+	}
 }
 
 function changeVolume() {
-;	console.log("Volume is ");
+	var slider = document.querySelector('#volumeSlider');
+	video.volume = slider.value / 100;
+	document.querySelector("#volume").innerHTML = video.volume * 100 + '%';
+	console.log("Volume is "+ video.volume*100+'%');
 }
        
 
 function gray() { 
-
+	video.style.filter= 'grayscale(100%)';
 	console.log("In grayscale")
 }
 
 function color() {
-
+	video.style.filter= 'grayscale(0%)';
 	console.log("In color") 
 }
